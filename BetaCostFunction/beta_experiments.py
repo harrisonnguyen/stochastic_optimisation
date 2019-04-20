c6f8d0ce289f38ce43f9104427eda57d905c3fc8
harrison <harrison.nguyen@sydney.edu.au>
2019-04-21 09:40:59 +1000

    added experiments to run

---
+++ BetaCostFunction/beta_experiments.py
@@ -0,0 +1,84 @@
+import sys
+sys.path.insert(0, '../')
+import numpy as np
+import scipy.stats as st
+from stochastic_optimisation.util import optimiser
+import argparse
+import os
+
+import pandas as pd
+
+def f_mean(*args,**kwargs):
+    return np.mean(f(*args,**kwargs),axis=0)
+
+def f(x,y,alpha,beta,gamma):
+    return x**2+alpha*x**2*y+beta*x*y**2+gamma*x*y
+
+def main(args):
+    file_name = os.path.join("BetaCostFunction",args.dist + "_" + args.file)
+    n_iter = args.n_iter
+    n_samples = args.samples
+
+
+    if args.dist == "beta1":
+        # Define the true distribution
+        dist = st.beta(a = 2,b=2,loc=-1,scale=2)
+    elif args.dist == "beta2":
+        # Define the true distribution
+        dist = st.beta(a = 5,b=5,loc=-1,scale=2)
+    elif args.dist == "beta3":
+        # Define the true distribution
+        dist = st.beta(a = 2,b=5,loc=-1,scale=2)
+
+
+    chosen_functions = [0,1,2,4,5,7,9,15,16,18]
+    parameter_bounds =None
+    #parameter_bounds =((1,None),(1,None),(None,None),(None,None))
+    cost_coefs = pd.read_csv(args.coef_file)
+
+    N = [10,25,50]
+    try:
+        df = pd.read_csv(file_name,index_col=0)
+    except:
+        df = pd.DataFrame(columns=['w_star','method','function',
+                        'N',"distribution"])
+        df.to_csv(file_name)
+
+    for z in range(len(chosen_functions)):
+        coefs = cost_coefs.iloc[chosen_functions[z]].values
+        new_f = lambda x,y: f_mean(x,y,coefs[0],coefs[1],coefs[2])
+        saa = optimiser.SAA(new_f)
+        bagging = optimiser.BaggingSolver(400,
+                                          objective_function=new_f)
+        mle = optimiser.MLESolver(st.beta,
+                                  parameter_bounds=parameter_bounds,
+                                  objective_function=new_f,
+                                 n_to_sample=n_samples,
+                                 floc=-1,
+                                 fscale=2)
+        bayes = optimiser.BetaBayesianSolver(objective_function=new_f,
+                                            n_to_sample=n_samples)
+        kde = optimiser.KDESolver(n_samples,
+                                  objective_function=new_f)
+        methods = [bagging,mle,saa,kde,bayes]
+        for k in range(n_iter):
+            for j in range(len(N)):
+                samples = dist.rvs(N[j])
+                for ele in methods:
+                    w_star = ele.solve(samples,initial_conditions=0.0)[0]
+                    results = {'w_star': w_star,'method':ele.__str__(),
+                           'function':chosen_functions[z]+1,'N':N[j],
+                           "distribution":args.dist}
+                    df = df.append(results,ignore_index=True)
+            # save the results so far
+            df.to_csv(file_name)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Perform portfolio experiments')
+    parser.add_argument('--samples', dest='samples',default=10000, type=int, help='number of samples for the computation of the expectation')
+    parser.add_argument('n_iter', type=int, help='number of iteration to perform experiments')
+    parser.add_argument('--file', dest='file', default='experiment_results.csv', help='name of the file to write/load')
+    parser.add_argument('--dist', dest='dist', help='underlying true distribution')
+    parser.add_argument('--coef_file', dest='coef_file', default='BetaCostFunction/cost_function_coeffs.csv', help='file where coefficients are located')
+    args = parser.parse_args()
+    main(args)
--- util/optimiser.py
+++ util/optimiser.py
@@ -4,6 +4,7 @@ import pymc3 as pm
 import pymc3.sampling as sampling
 import theano
 from sklearn.utils import resample
+from scipy.misc import logsumexp
 
 from scipy.stats import gaussian_kde
 class SAA(object):
@@ -55,14 +56,30 @@ class BaggingSolver(SAA):
         return 'Bagging'
 
 class MLESolver(SAA):
-    def __init__(self,distribution,n_to_sample=5000,*args,**kwargs):
+    def __init__(self,distribution,parameter_bounds=None,
+                n_to_sample=5000,*args,**kwargs):
+        self.floc = kwargs.pop("floc",None)
+        self.fscale = kwargs.pop("fscale",None)
         super(MLESolver,self).__init__(*args,**kwargs)
         self.n_to_sample = n_to_sample
         self.distribution = distribution
+        self.parameter_bounds = parameter_bounds
+
 
     def solve(self,samples,*args,**kwargs):
-        parameters = self.distribution.fit(samples)
+
+        parameters = self.distribution.fit(samples,floc=self.floc,fscale=self.fscale)
+
+        if self.parameter_bounds is not None:
+            def beta_loglikelihood(params,x):
+                log_pdf = self.distribution.logpdf(x, params[0], params[1], loc=params[2], scale=params[3])
+                return -logsumexp(log_pdf,axis=0)
+            parameters = minimize(beta_loglikelihood,parameters,args=(samples),
+                                bounds=self.parameter_bounds,options={'maxiter':1000}).x
+
+
         new_samples = self.distribution(*parameters).rvs(self.n_to_sample)
+
         return super(MLESolver,self).solve(new_samples,*args,**kwargs)
 
     def __str__(self):
@@ -87,7 +104,7 @@ class BetaBayesianSolver(SAA):
 
             self.shared_data.set_value(scaled_samples)
             # Sample from the posterior using the sampling method
-            trace = pm.sample(self.n_to_sample, step=self.step, njobs=4,progressbar=False,cores=4)
+            trace = pm.sample(self.n_to_sample, step=self.step, njobs=4,progressbar=False,cores=4,verbose=-1)
         obs = sampling.sample_ppc(trace,samples=50,model=self.model,size=500,progressbar=False)
         new_samples = np.reshape(obs['obs'],-1,1)*scale+loc
         return super(BetaBayesianSolver,self).solve(new_samples,*args,**kwargs)
